18524493c69ce0ee0a30197b098a2e7618c007c6
liuhjhj <liu2445342557@gmail.com>
2022-04-10 21:50:38 +0800

    backup code

--- src/string/CountAndSay.java
+++ src/string/CountAndSay.java
@@ -8,4 +8,29 @@ package string;
  * @date 2022/4/10
  **/
 public class CountAndSay {
+
+    public static String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder("1");
+        for (int i = 1; i < n; i++) {
+            String s = sb.toString();
+            sb.setLength(0);
+            for (int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                int count = 1;
+                for (int k = j + 1; k < s.length() && s.charAt(k) == s.charAt(j); k++){
+                    count++;
+                    j++;
+                }
+                sb.append(count).append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 31; i++) {
+            String s = countAndSay(i);
+            System.out.println(s);
+        }
+    }
 }
